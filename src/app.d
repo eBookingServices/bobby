87e8b17c465e194a0f3228533defdfc6f06fab27
Márcio Martins <marcioapm@gmail.com>
2015-09-10 22:09:13 +0100

    Initial commit

---
+++ src/app.d
@@ -0,0 +1,309 @@
+import core.thread;
+
+import std.algorithm;
+import std.array;
+import std.c.stdlib;
+import std.conv;
+import std.datetime;
+import std.file;
+import std.getopt;
+import std.json;
+import std.path;
+import std.process;
+import std.range;
+import std.string;
+import std.stdio;
+
+
+import monitor;
+
+
+
+struct App {
+	string exe;
+	string[] args;
+	string workingDir;
+	string stdoutFileName;
+	string stderrFileName;
+	string pidFileName;
+
+	string httpMonitorURL;
+
+	size_t flags;
+	size_t starts;
+	SysTime started;
+	size_t delay;
+
+	HTTPMonitor httpMonitor;
+
+	Pid pid;
+	File stdout;
+	File stderr;
+}
+
+
+void bark(string error) {
+	std.stdio.stderr.write(format("[%s bark] %s\n", Clock.currTime.toSimpleString, error));
+}
+
+
+private size_t parseAppFlag(string value) {
+	switch (value.toLower) {
+	default: return 0;
+	}
+}
+
+
+bool isExternal(string path) {
+	return !path.empty && !path.isAbsolute && (path.length > 1) && (path[0..2] == "..");
+}
+
+
+string fix(string path) {
+	if (path.isAbsolute) {
+		auto relative = path.relativePath(cwd_);
+		if (!relative.isExternal)
+			path = relative;
+	}
+
+	string result = buildNormalizedPath(path);
+	if (!result.length)
+		result = ".";
+	return result;
+}
+
+
+string absolute(string path) {
+	if (!path.isAbsolute)
+		path = buildNormalizedPath(cwd_, path);
+	return path;
+}
+
+
+void killit(ref App app) {
+	app.killMonitors;
+
+	if (app.pid) {
+		try {
+			bark(format("killing %s (%d)...", baseName(app.exe), app.pid.processID));
+
+			version(Posix)app.pid.kill(SIGKILL);
+			else app.pid.kill;
+
+			app.pid.wait;
+			app.pid.destroy;
+		} catch (Throwable e) {
+			bark(e.msg);
+		}
+
+		try {
+			if (!app.pidFileName.empty && exists(app.pidFileName))
+				remove(app.pidFileName);
+		} catch (Throwable e) {
+			bark(e.msg);
+		}
+	}
+}
+
+
+void startMonitors(ref App app) {
+	if (!app.httpMonitorURL.empty) {
+		app.httpMonitor.start(app.httpMonitorURL);
+		bark(format("HTTP monitor started for %s (%d) (%s)...", baseName(app.exe), app.pid.processID, app.httpMonitorURL));
+	}
+}
+
+void killMonitors(ref App app) {
+	if (!app.httpMonitorURL.empty)
+		app.httpMonitor.reset;
+}
+
+void updateMonitors(ref App app) {
+	if (!app.httpMonitorURL.empty)
+		app.httpMonitor.update;
+}
+
+
+bool alive(ref App app) {
+	if (app.pid is null)
+		return false;
+
+	auto status = tryWait(app.pid);
+	if (status.terminated) {
+		app.pid.destroy;
+		app.pid = null;
+		return false;
+	}
+
+	if (!app.httpMonitorURL.empty && (app.httpMonitor.state == HTTPMonitor.State.Failure)) {
+		bark("HTTP failed to get " ~ app.httpMonitorURL);
+		return false;
+	}
+
+	return true;
+}
+
+
+void spawn(ref App app) {
+	auto execute = app.exe;
+	auto workingDir = app.workingDir.length ? app.workingDir : cwd_;
+	if (!workingDir.isAbsolute)
+		workingDir = buildNormalizedPath(cwd_, workingDir);
+
+	auto args = [ execute ] ~ app.args;
+
+	try {
+		args[0] = args[0].absolute;
+
+		++app.starts;
+		app.started = Clock.currTime;
+
+		app.pid = spawnProcess(args, std.stdio.stdin, app.stdout, app.stderr, null, Config.inheritFDs | Config.retainStdout | Config.retainStderr, workingDir);
+	} catch(Throwable e) {
+		bark(e.msg);
+	}
+
+	if (app.alive) {
+		bark(format("app started %s (%d) (x%d)...", baseName(app.exe), app.pid.processID, app.starts));
+
+		app.startMonitors;
+
+		if (!app.pidFileName.empty) {
+			try {
+				std.file.write(app.pidFileName, app.pid.processID.to!string);
+			} catch (Throwable e) {
+				bark(e.msg);
+			}
+		}
+	} else {
+		bark(format("failed to start %s...", baseName(app.exe)));
+	}
+}
+
+extern(C) void signalHandler(int sig) {
+	try {
+		app_.killit;
+
+		std.stdio.stdout.flush;
+		std.stdio.stderr.flush;
+	} catch {
+	}
+
+	exit(0);
+}
+
+version(Posix) {
+	import core.sys.posix.signal;
+	void installSignalHandlers() {
+		sigset_t sigset;
+		sigemptyset(&sigset);
+		sigaction_t siginfo;
+		siginfo.sa_handler = &signalHandler;
+		siginfo.sa_mask = sigset;
+		siginfo.sa_flags = 0;
+
+		sigaction(SIGKILL, &siginfo, null);
+		sigaction(SIGINT, &siginfo, null);
+		sigaction(SIGTERM, &siginfo, null);
+	}
+} else {
+	import core.stdc.signal;
+	void installSignalHandlers() {
+		import std.traits;
+		signal(SIGABRT, cast(ParameterTypeTuple!signal[1])&signalHandler);
+		signal(SIGTERM, cast(ParameterTypeTuple!signal[1])&signalHandler);
+		signal(SIGINT, cast(ParameterTypeTuple!signal[1])&signalHandler);
+	}
+}
+
+
+__gshared App app_;
+__gshared string cwd_;
+
+
+int main(string[] args) {
+	installSignalHandlers;
+
+	cwd_ = buildNormalizedPath(getcwd);
+
+	bool force = false;
+
+	string pidFile;
+	string stdoutFile;
+	string stderrFile;
+	string workingDir;
+	string httpMonitorURL;
+
+
+	try {
+		auto opts = getopt(args,
+			"f|force", "Force execution even if pid file is still present", &force,
+			"p|pid-file", "Path of the .pid file", &pidFile,
+			"o|stdout-file", "File to use as stdout - can be the same as stderr", &stdoutFile,
+			"e|stderr-file", "File to use as stderr - can be the same as stdout", &stderrFile,
+			"w|working-dir", "Working directory", &workingDir,
+			"h|monitor-http-url", "URL to monitor for HTTP availability", &httpMonitorURL);
+
+		if (opts.helpWanted || (args.length < 2)) {
+			defaultGetoptPrinter("Usage: bobby [OPTIONS] executable\n", opts.options);
+			return 1;
+		}
+	} catch (Exception e) {
+		writeln("Error: ", e.msg);
+		return 1;
+	}
+
+    app_ = App(args[1], args[1..$], workingDir, stdoutFile, stderrFile, pidFile, httpMonitorURL);
+
+	try {
+        if (!app_.stdoutFileName.empty) {
+            app_.stdout.open(app_.stdoutFileName, "a");
+        } else {
+            app_.stdout = std.stdio.stdout;
+        }
+
+        if (!app_.stderrFileName.empty) {
+            if (app_.stderrFileName == app_.stdoutFileName) {
+                app_.stderr = app_.stdout;
+            } else {
+                app_.stderr.open(app_.stderrFileName, "a");
+            }
+        } else {
+            app_.stderr = std.stdio.stderr;
+        }
+
+        if (!app_.pidFileName.empty && exists(app_.pidFileName)) {
+            if (!force) {
+                bark(format("%s exists - the process may be running!\nRemove it manually or use --force to ignore it.", app_.pidFileName));
+                return 1;
+            }
+
+            bark(format("deleting %s...", app_.pidFileName));
+            remove(app_.pidFileName);
+        }
+	} catch (Throwable e) {
+		bark(e.msg);
+		return 1;
+	}
+
+	while (true) {
+        if (!app_.alive) {
+            if (!app_.starts) {
+                app_.spawn;
+            } else {
+                if ((Now - app_.started) > app_.delay.seconds) {
+                    app_.killit;
+                    app_.spawn;
+                    app_.delay = min(max(1, app_.delay) * 2, 60);
+                }
+            }
+        } else {
+            if ((Clock.currTime - app_.started) > 2.seconds)
+                app_.delay = 0;
+
+            app_.updateMonitors;
+        }
+
+		Thread.sleep(100.msecs);
+	}
+}
---
+++ src/monitor.d
@@ -0,0 +1,282 @@
+module monitor;
+
+import core.memory;
+
+import std.array;
+import std.conv;
+import std.datetime;
+import std.regex;
+import std.socket;
+import std.string;
+import std.stdio;
+
+
+alias Now = Clock.currTime;
+
+
+struct URL {
+	static URL parse(string url) {
+		auto matches = matchFirst(url, ctRegex!(r"^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?$", "i"));
+
+		URL result;
+		if (!matches.empty) {
+			result.protocol = matches[2].toLower;
+			auto host = matches[4].toLower;
+			auto index = host.indexOf(':');
+
+			if (index != -1) {
+				result.port = host[index + 1..$].to!ushort;
+				result.host = host[0..index];
+			} else {
+				switch(result.protocol) {
+				case "http":
+					result.port = 80;
+					break;
+				case "https":
+					result.port = 443;
+					break;
+				default:
+					break;
+				}
+				result.host = host;
+			}
+
+			result.path = matches[5];
+			result.query = matches[7];
+			result.anchor = matches[9];
+		}
+
+		return result;
+	}
+
+	string protocol;
+	string host;
+	ushort port;
+	string path;
+	string query;
+	string anchor;
+}
+
+
+struct HTTPMonitor {
+	enum State : uint {
+		Reset,
+		Ready,
+		Idle,
+		Connect,
+		Connecting,
+		Connected,
+		Request,
+		Requesting,
+		Requested,
+		Success,
+		Failure,
+		GraceFailure,
+	}
+
+	~this() {
+		reset();
+	}
+
+	void reset() {
+		close();
+		try_ = 0;
+
+		changeState(State.Reset);
+	}
+
+	void start(string url) {
+		url_ = URL.parse(url);
+
+		close();
+		try_ = 0;
+
+		changeState(State.Ready);
+		monitorStart_ = Now;
+	}
+
+	private void close() {
+		if (socket_ !is null) {
+			socket_.shutdown(SocketShutdown.SEND);
+			socket_.close();
+
+			destroy(socket_);
+			socket_ = null;
+
+			GC.collect();
+		}
+	}
+
+	private void changeState(State state) {
+		state_ = state;
+	}
+
+	void update() {
+		final switch (state_) with (State) {
+		case Reset:
+			break;
+
+		case Ready:
+			changeState(Connect);
+			goto case Connect;
+
+		case Idle:
+			if (((Now - testStart_).total!"msecs" >= intervalMS_)) {
+				changeState(Connect);
+				goto case Connect;
+			}
+			break;
+
+		case Connect:
+			assert(socket_ is null);
+			try {
+				testStart_ = Now;
+				socket_ = new TcpSocket();
+				socket_.blocking = false;
+
+				socket_.connect(new InternetAddress(url_.host, url_.port));
+
+				changeState(Connecting);
+				goto case Connecting;
+			} catch (Throwable e) {
+				writeln(e.toString);
+				changeState(Failure);
+				goto case Failure;
+			}
+
+		case Connecting:
+			changeState(Connected);
+			goto case Connected;
+
+		case Connected:
+			changeState(Request);
+			goto case Request;
+
+		case Request:
+			if (request_.empty) {
+				auto app = appender!string;
+
+				app.put("GET ");
+				app.put(url_.path.empty ? "/" : url_.path);
+				if (!url_.query.empty) {
+					app.put("?");
+					app.put(url_.query);
+				}
+				app.put(" HTTP/1.1\r\n\r\n");
+
+				request_ = cast(ubyte[])app.data.idup;
+			}
+
+			try {
+				auto result = socket_.send(request_);
+
+				if (result == Socket.ERROR) {
+					if ((Now - testStart_).total!"msecs" >= timeoutMS_) {
+						changeState(Failure);
+						goto case Failure;
+					}
+					break;
+				}
+
+				received_.length = 0;
+
+				changeState(Requesting);
+				goto case Requesting;
+			} catch {
+				changeState(Failure);
+				goto case Failure;
+			}
+
+		case Requesting:
+			ubyte[32] buf;
+
+			auto result = socket_.receive(buf);
+			if (result > 0) {
+				received_ ~= buf[0..result];
+				if (received_.length > 12) {
+					changeState(Requested);
+					goto case Requested;
+				}
+			} else if (result == Socket.ERROR) {
+				if ((Now - testStart_).total!"msecs" >= timeoutMS_) {
+					changeState(Failure);
+					goto case Failure;
+				}
+				break;
+			} else if (result == 0) {
+				changeState(Requested);
+				goto case Requested;
+			}
+			break;
+
+		case Requested:
+			if (statusOK()) {
+				changeState(Success);
+				goto case Success;
+			}
+			changeState(Failure);
+			goto case Failure;
+
+		case Success:
+			close();
+			try_ = 0;
+
+			changeState(Idle);
+			goto case Idle;
+
+		case Failure:
+			close();
+			++try_;
+
+			if ((try_ <= retryCount_) || ((Now - monitorStart_).total!"msecs" <= graceMS_)) {
+				changeState(GraceFailure);
+				goto case GraceFailure;
+			}
+			break;
+
+		case GraceFailure:
+			if ((Now - monitorStart_).total!"msecs" <= graceMS_)
+				try_ = 0;
+
+			changeState(Idle);
+			goto case Idle;
+		}
+	}
+
+	private bool statusOK() {
+		if (received_.length <= 12)
+			return false;
+
+		if ((received_.ptr[0] != 'H') || (received_.ptr[1] != 'T') || (received_.ptr[2] != 'T') || (received_.ptr[3] != 'P') || (received_.ptr[4] != '/'))
+			return false;
+
+		if (received_.ptr[8] != ' ')
+			return false;
+
+		if (received_.ptr[9] == '5')
+			return false;
+		return true;
+	}
+
+	@property State state() {
+		return state_;
+	}
+
+private:
+	URL url_;
+
+	size_t timeoutMS_ = 2500;
+	size_t intervalMS_ = 5000;
+	size_t graceMS_  = 25000;
+	size_t retryCount_ = 3;
+	size_t try_ = 0;
+
+	ubyte[] request_;
+	ubyte[] received_;
+
+	SysTime monitorStart_;
+	SysTime testStart_;
+
+	State state_ = State.Ready;
+	Socket socket_;
+}
